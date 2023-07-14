429061d27317a2f481cbf39006f93ed47c974b13
Signs Madueke <signsmadueke1@gmail.com>
2023-07-14 19:04:15 +0100

    ...

--- free-gift.php
+++ free-gift.php
@@ -192,7 +192,7 @@
         <img class="svg decor" src="assets/images/icons/decor.svg">
     </div>
 
-    <form method="post" action="https://sendfox.com/form/1gz86j/m5ev88" class="sendfox-form" id="m5ev88" data-async="true" data-recaptcha="false">
+    <form method="post" action="https://sendfox.com/form/1gz86j/m5ev88" class="sendfox-form" id="m5ev88" data-async="true" data-recaptcha="true">
         <div class="form-inputs">
             <div class="name">
                 <div class="sendfox-group animate reveal">
@@ -220,6 +220,17 @@
     </form>
     <script src="https://sendfox.com/js/form.js"></script>			
 </section>
+
+<form method="post" action="https://sendfox.com/form/36p4oo/3ew4vl" class="sendfox-form" id="3ew4vl" data-async="true" data-recaptcha="true">
+<p><label for="sendfox_form_name">First Name: </label><input type="text" id="sendfox_form_name" placeholder="First Name" name="first_name" required /></p>
+<p><label for="sendfox_form_last_name">Last Name: </label><input type="text" id="sendfox_form_last_name" placeholder="Last Name" name="last_name" required /></p>
+<p><label for="sendfox_form_email">Email: </label><input type="email" id="sendfox_form_email" placeholder="Email" name="email" required /></p>
+<!-- no botz please -->
+<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="a_password" tabindex="-1" value="" autocomplete="off" /></div>
+<p><button type="submit">Send my free boxset!</button></p>
+</form>
+<script src="https://sendfox.com/js/form.js"></script>
+			
 
 <footer id="footer" class="container" style="padding: 30px; grid-template-columns: 1fr 1fr;">
     <p class="copyright animate reveal">&copy; 2020 Chima Ugochukwu. All rights reserved.</p>
